e00d5264cbbd78d9ba7d1b8649b60be0a43ab92b
WangWang0226 <eeha8834@gmail.com>
2025-01-14 19:29:58 -0500

    feat[frontend]: enable user approve and deposit to vault

---
+++ nextjs/src/app/abi/vaultAbi.js
@@ -0,0 +1,390 @@
+const VAULT_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "premium",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "maxUnits",
+				"type": "uint256"
+			}
+		],
+		"name": "AdjustOption",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "cashAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "Deposit",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [],
+		"name": "ExerciseOption",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "baseTokenAmount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "quoteTokenAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "Withdraw",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "adminWithdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "baseToken",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "cashAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "expiry",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "factory",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "finalTotalUnits",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_baseToken",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_quoteToken",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_oracle",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_expiry",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_strikePrice",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_premium",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_maxUnits",
+				"type": "uint256"
+			}
+		],
+		"name": "init",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "isAdmin",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isInitialized",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "maxUnits",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "oracle",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "premium",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "quoteToken",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "settle",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "state",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "strikePrice",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalUnits",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "baseTokenAmount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "quoteTokenAmount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+export default VAULT_ABI;
--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -4,12 +4,13 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount, useConnect, useDisconnect, useBalance, useWriteContract, useSimulateContract, useWaitForTransactionReceipt } from "wagmi";
 import { formatUnits } from "ethers";
 import FAUCET_ABI from './abi/faucetAbi.js';
+import VAULT_ABI from './abi/vaultAbi.js';
+import ERC20_ABI from './abi/ERC20Abi.js';
 
-const FAUCET_CONTRACT_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"; // 替換為你的 Faucet 合約地址
-
-const TOKEN0_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // 替換為你的 Token0 地址（WETH）
-const TOKEN1_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // 替換為你的 Token1 地址（USDC）
-
+const FAUCET_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_FAUCET_CONTRACT_ADDRESS;
+const TOKEN0_ADDRESS = process.env.NEXT_PUBLIC_TOKEN0_ADDRESS
+const TOKEN1_ADDRESS = process.env.NEXT_PUBLIC_TOKEN1_ADDRESS
+const VAULT_ADDRESS = process.env.NEXT_PUBLIC_VAULT_ADDRESS
 
 export default function HomePage() {
   const { address, isConnected } = useAccount({
@@ -27,16 +28,41 @@ export default function HomePage() {
   const { disconnect } = useDisconnect();
   const [balanceToken0, setBalanceToken0] = useState("0");
   const [balanceToken1, setBalanceToken1] = useState("0");
+  const [vaultBalanceToken0, setVaultBalanceToken0] = useState("0");
+  const [vaultBalanceToken1, setVaultBalanceToken1] = useState("0");
+
   const [transactionHash, setTransactionHash] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [cashAmount, setCashAmount] = useState("");
 
 
-  const { data } = useSimulateContract({
+
+  const { data: claimTokenData } = useSimulateContract({
     address: FAUCET_CONTRACT_ADDRESS,
     abi: FAUCET_ABI,
     functionName: "claimToken",
+  });
+
+  const { data: approveDepositData } = useSimulateContract({
+    address: TOKEN1_ADDRESS, //USDC
+    abi: ERC20_ABI,
+    functionName: "approve",
+    args: [
+      VAULT_ADDRESS,
+      cashAmount ? BigInt(cashAmount * 10 ** 6) : BigInt(0), // Convert user input to BigInt
+    ],
+  });
+
+  const { data: depositData } = useSimulateContract({
+    address: VAULT_ADDRESS,
+    abi: VAULT_ABI,
+    functionName: "deposit",
+    args: [
+      address,
+      cashAmount ? BigInt(cashAmount * 10 ** 6) : BigInt(0), // Convert user input to BigInt
+    ],
   });
 
   const { writeContract } = useWriteContract({
@@ -52,13 +78,25 @@ export default function HomePage() {
     hash: transactionHash,
   });
 
-  
+  // Handle user input change
+  const handleInputChange = (event) => {
+    setCashAmount(event.target.value);
+    console.log("input cashAmount: ", event.target.value)
+  };
+
+  const isAmountValid = cashAmount && parseFloat(cashAmount) > 0; // 驗證金額是否有效
+
+
   // 提取並同步交易狀態
   useEffect(() => {
     if (result) {
       setIsLoading(result.isLoading);
       setIsSuccess(result.isSuccess);
       setIsError(result.isError);
+      if (result.isSuccess) {
+        refetchBalances()
+        refetchVaultBalances()
+      }
     }
   }, [result]);
 
@@ -76,6 +114,26 @@ export default function HomePage() {
     token: TOKEN1_ADDRESS,
     watch: true,
   });
+
+  // 獲取 Vault Token0（WETH）的餘額
+  const { data: vaultToken0BalanceData, refetch: refetchVaultToken0 } = useBalance({
+    address: VAULT_ADDRESS,
+    token: TOKEN0_ADDRESS,
+    watch: true,
+  });
+
+  // 獲取 Vault Token0（WETH）的餘額
+  const { data: vaultToken1BalanceData, refetch: refetchVaultToken1 } = useBalance({
+    address: VAULT_ADDRESS,
+    token: TOKEN1_ADDRESS,
+    watch: true,
+  });
+
+  // 定義更新餘額的方法
+  const refetchVaultBalances = () => {
+    refetchVaultToken0();
+    refetchVaultToken1();
+  };
 
   // 定義更新餘額的方法
   const refetchBalances = () => {
@@ -101,6 +159,25 @@ export default function HomePage() {
     }
   }, [token1BalanceData]);
 
+  // 更新 Vault Token0 餘額
+  useEffect(() => {
+    if (vaultToken0BalanceData?.value !== undefined && vaultToken0BalanceData?.decimals !== undefined) {
+      setVaultBalanceToken0(formatUnits(vaultToken0BalanceData.value, vaultToken0BalanceData.decimals));
+    } else {
+      console.warn("Vault Token0 balance data is incomplete:", vaultToken0BalanceData);
+    }
+  }, [vaultToken0BalanceData]);
+
+
+  useEffect(() => {
+    if (vaultToken1BalanceData?.value !== undefined && vaultToken1BalanceData?.decimals !== undefined) {
+      setVaultBalanceToken1(formatUnits(vaultToken1BalanceData.value, vaultToken1BalanceData.decimals));
+    } else {
+      console.warn("Vault Token1 balance data is incomplete:", vaultToken1BalanceData);
+    }
+  }, [vaultToken1BalanceData]);
+
+
 
   return (
     <div>
@@ -111,21 +188,55 @@ export default function HomePage() {
           <div>
             <p>Connected to: {address}</p>
             <button onClick={disconnect}>Disconnect</button>
-            <p>Token0 (WETH) Balance: {balanceToken0}</p>
-            <p>Token1 (USDC) Balance: {balanceToken1}</p>
+            <p>Your Token0 (WETH) Balance: {balanceToken0}</p>
+            <p>Your Token1 (USDC) Balance: {balanceToken1}</p>
+            <p>Vault's Token0 (WETH) Balance: {vaultBalanceToken0}</p>
+            <p>Vault's Token1 (USDC) Balance: {vaultBalanceToken1}</p>
 
             <div>
               <button
-                disabled={!Boolean(data?.request)}
-                onClick={() => writeContract(data.request)}
+                disabled={!Boolean(claimTokenData?.request)}
+                onClick={() => writeContract(claimTokenData.request)}
               >
                 Claim Tokens
               </button>
-              {isLoading && <p>Transaction in progress...</p>}
-              {isSuccess && <p>Transaction succeeded!</p>}
-              {isError && <p>Transaction failed.</p>}
+            </div>
+
+            <div>
+              {!isAmountValid && (
+                <p style={{ color: "red", fontSize: "14px" }}>
+                  Please enter a valid amount greater than 0.
+                </p>
+              )}
+              <label>
+                Enter cash amount (e.g., USDC):
+                <input
+                  type="number"
+                  value={cashAmount}
+                  onChange={handleInputChange}
+                  placeholder="Enter amount"
+                />
+              </label>
+
+              <button
+                disabled={!isAmountValid || !Boolean(approveDepositData?.request)} // 新增驗證條件
+                onClick={() => writeContract(approveDepositData.request)}
+              >
+                Approve deposit with {cashAmount} USDC to Vault
+              </button>
+
+              <button
+                disabled={!isAmountValid || !Boolean(depositData?.request)} // 新增驗證條件
+                onClick={() => writeContract(depositData.request)}
+              >
+                Deposit
+              </button>
+
             </div>
           </div>
+          {isLoading && <p style={{ color: "blue", fontSize: "14px" }}>Transaction in progress...</p>}
+          {isSuccess && <p style={{ color: "green", fontSize: "14px" }}>Transaction succeeded!</p>}
+          {isError && <p style={{ color: "red", fontSize: "14px" }}>Transaction failed.</p>}
         </div>
 
 
