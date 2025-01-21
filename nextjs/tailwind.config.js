3e02c630eeb9b877f513ddeb9a2437428a8359fa
WangWang0226 <eeha8834@gmail.com>
2025-01-21 02:36:45 -0500

    feat[docker]: update docker config

---
+++ nextjs/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fbbf24',
+        secondary: '#3965a2',
+        deepBlue: '#38538d'
+      },
+      backgroundImage: {
+        'wallpaper': "url('/bg-photo.jpeg')",
+        'wallpaper2': "url('/bg-photo2.jpeg')",
+        'wallpaper3': "url('/bg-photo3.jpeg')",
+        'projectIcon': "url('/projectIcon.jpeg')",
+      }
+    },
+  },
+  plugins: [],
+};
