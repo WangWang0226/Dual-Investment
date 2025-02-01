aa58edaab18aae8197f266a69406e44c17ebc7ee
WangWang0226 <eeha8834@gmail.com>
2025-02-01 16:44:14 -0500

    update

--- expressjs/api/server.js
+++ expressjs/api/server.js
@@ -28,7 +28,11 @@ const walletClient = createWalletClient({
     account: privateKeyToAccount(process.env.OWNER_PRIVATE_KEY), 
 });
 
-app.get("/api", (req, res) => res.send("Express running on Vercel!!"));
+app.get("/api/hello", (req, res) => {
+    res.json({
+        message: "Express running on Vercel!!"
+    })
+});
 
 app.post('/api/callContract', async (req, res) => {
     try {
